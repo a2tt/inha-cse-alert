53a5264b3006fadebca71706fa58e4a90bd107e7
usera2tt <usera2tt@gmail.com>
2021-02-23 13:16:20 +0900

    인하대 공지 추가

--- configs.py
+++ configs.py
@@ -5,7 +5,7 @@ class CrawlUrl(Enum):
     학생회소식 = 'https://cse.inha.ac.kr/cse/884/subview.do'
     공지사항 = 'https://cse.inha.ac.kr/cse/888/subview.do'
     졸업예정자공지 = 'https://cse.inha.ac.kr/cse/889/subview.do'
-
+    인하대공지 = 'https://www.inha.ac.kr/kr/950/subview.do'
 
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36'
 
