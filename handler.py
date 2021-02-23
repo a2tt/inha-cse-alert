2b397014fc46ee9f5939adc80007cdb37af9cd81
usera2tt <usera2tt@gmail.com>
2021-02-23 13:16:42 +0900

    에러 발생 시 텔레그램 알림

--- handler.py
+++ handler.py
@@ -1,6 +1,18 @@
+import traceback
+
+from helpers import telegram_bot
+
+import settings
+
+
 def board_crawler(event=None, context=None):
     from tasks.board_crawler import main
-    return main()
+    try:
+        return main()
+    except:
+        e_str = traceback.format_exc()
+        telegram_bot.send_message(e_str, settings.TELEGRAM_ALERT_CHANNEL_ID)
+        return
 
 
 if __name__ == '__main__':
