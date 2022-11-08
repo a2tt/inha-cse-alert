42a3666b1cb6d29b7326993d0686bd368ff08968
a2tt <usera2tt@gmail.com>
2022-11-09 00:15:22 +0900

    fix: add IAM for eventbridge source

--- terraform/application/eventbridge.tf
+++ terraform/application/eventbridge.tf
@@ -18,3 +18,33 @@ resource "aws_cloudwatch_event_target" "task" {
   event_bus_name = data.aws_cloudwatch_event_bus.default.arn
   arn            = aws_lambda_function.this.arn
 }
+
+# IAM policy for Lambda to be triggered by eventbridge
+resource "aws_iam_policy" "eventbridge_target" {
+  name        = "${local.name_prefix}-policy-eventbridgeTarget"
+  path        = "/"
+  description = "IAM policy to allow eventbridge to trigger lambda"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "lambda:InvokeFunction",
+      "Resource": "*",
+      "Condition": {
+        "StringEquals": {
+          "aws:SourceArn": "${aws_cloudwatch_event_rule.cron.arn}"
+        }
+      }
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "eventbridge_target" {
+  role       = aws_iam_role.lambda.name
+  policy_arn = aws_iam_policy.eventbridge_target.arn
+}
